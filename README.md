a666b2b78f7d91a879ed3e65d80f480051f60761
Mabel Duran Sanchez <you@example.com>
2020-09-10 07:46:19 -0400

    Completed levels 1 and 2 and edited readme.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # javascript-challenge
 JavaScript and DOM Manipulation HW
+
+For this assignment, we had to create a table which dynamically filters through a UFO sightings dataset based on the date (date/time) of the sighting input by users (for the UFO-level-1 section of the assignment). For the second part of the assigment, we added multiple inputs tags so that users can filter the original dataset of UFO sightings by multiple filters including date/time, city, state, country, and shape.
---
+++ UFO-level-2/static/js/app_bonus.js
@@ -0,0 +1,73 @@
+// TO MAKE TABLE VIEWABLE ON SITE
+
+// From data.js
+var tableData = data;
+
+// // Print data in the console
+// console.log(tableData)
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Use d3 to append one table row `tr` for each data object
+tableData.forEach(function (UFOs) {
+    console.log(UFOs);
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to console.log each data value
+    Object.entries(UFOs).forEach(function ([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// TO MAKE TABLE ON SITE INTERACTIVE WITH SEVERAL FILTERS
+
+// Select the all the list elements with the "filter list-group-item" class
+var filter = d3.selectAll(".filter");
+
+// Create a change event function
+filter.on("change", createChange);
+
+inputdict = {}
+
+function createChange() {
+    var filteredData = tableData
+    var inputselect = d3.select(this).select("input");
+    var selectedInput = inputselect.property("value");
+    var idselect = inputselect.attr("id");
+
+    inputdict[idselect] = selectedInput;
+    console.log(inputdict);
+
+    Object.entries(inputdict).forEach(([key, value]) => {
+        filteredData = filteredData.filter(x => x[key] === value);
+
+        console.log(filteredData);
+
+    // while(Object.entries(inputdict).forEach(([key, value]) => {
+    //     filteredData = filteredData.filter(x => x[key] === value)) {console.log(filteredData);
+
+// Remove any children from the tbody        
+        tbody.html("");
+
+// Use d3 to append one table row `tr` for each data object
+        filteredData.forEach(function (UFOs) {
+            console.log(UFOs);
+            var row = tbody.append("tr");
+        
+// Use `Object.entries` to console.log each data value
+            Object.entries(UFOs).forEach(function ([key, value]) {
+                console.log(key, value);
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        }); 
+        
+    //     else {console.log(`No search results found matching your search criteria. Please try again.`);}
+    
+    // };
+
+    })
+};
